bbaa45130574a415c4e54aa982b5a227083951a6
Aditya Upadhayay <adityaupadhayay066@gmail.com>
2026-01-11 16:23:26 +0530

    Alarm clock using python

--- Alarm_clock.py
+++ Alarm_clock.py
@@ -3,26 +3,37 @@ import datetime
 import pygame
 
 def set_alarm(alarm_time):
-    print(f"Alarm set for {alarm_time}")
-    sound_file = "MY_music.mp3"
+    print(f"Alarm time set for {alarm_time}")
+    sound_file = "Alarm_sound.mp3"
     is_running = True
+
+    pygame.mixer.init()
+
+    alarm_time_dt = datetime.datetime.strptime(alarm_time, "%H:%M:%S")
 
     while is_running:
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        print("Current time:", current_time)
+        print("Current time :", current_time)
 
-        if (current_time >= alarm_time):
-            print("Wake Up buddy 😊")
+        # convert current_time string → datetime
+        current_time_dt = datetime.datetime.strptime(current_time, "%H:%M:%S")
 
-            pygame.mixer.init()
+        if current_time_dt >= alarm_time_dt:
+            print("     Wake Up buddy 😊       :")
+
             pygame.mixer.music.load(sound_file)
-            pygame.mixer.music.play() 
+            pygame.mixer.music.play()
 
+            start = time.time()
             while pygame.mixer.music.get_busy():
+                if time.time() - start >= 30:  
+                    pygame.mixer.music.stop()
+                    break
                 time.sleep(1)
 
-            is_running = False 
-        time.sleep(1) 
+            is_running = False
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time (HH:MM:SS): ")
